09dc246e3515abf6fdca4b75286a288c682e4425
Calvin Lee <calvin.hanbee.lee@gmail.com>
2023-09-05 17:31:44 +1200

    Add print string for lineup.py

--- lineup.py
+++ lineup.py
@@ -16,3 +16,13 @@ class Lineup:
         self.positioning_image_url = positioning_image_url
         self.aim_image_url = aim_image_url
         self.landing_image_url = landing_image_url
+
+    def __str__(self):
+        return (
+            f"Agent: {self.agent}\n"
+            f"Map: {self.map}\n"
+            f"Site: {self.site}\n"
+            f"Positioning Url: {self.positioning_image_url}\n"
+            f"Aim Url: {self.aim_image_url}\n"
+            f"Landing Url: {self.landing_image_url}"
+        )
